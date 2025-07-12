90f77d45e9d738129352681e9c983e8b65080010
Медведкин Сергей <meves.sergey@gmail.com>
2025-07-12 16:46:12 +0300

    Удалены подмодули и добавлены как обычные директории

---
+++ apps/mfe-next/src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Next MFE</h1>
+    </div>
+  );
+}
---
+++ apps/shell/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>🧩 Shell: стартовая страница</h1>
+      
+      <nav>
+        <ul>
+          <li><a href="http://127.0.0.1:3001" target="_blank" rel="noopener noreferrer">MFE Next.js</a></li>
+          <li><a href="http://127.0.0.1:3002" target="_blank" rel="noopener noreferrer">MFE Solid</a></li>
+        </ul>
+      </nav>
+    </div>
+  ) 
+}
