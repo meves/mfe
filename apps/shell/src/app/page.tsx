fb384bbd171d5d6b8cc7674002e0ef5762108bfc
Медведкин Сергей <meves.sergey@gmail.com>
2025-07-13 11:14:02 +0300

    plug in postgres database to svelte kit

--- apps/shell/src/app/page.tsx
+++ apps/shell/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function HomePage() {
         <ul>
           <li><a href="http://127.0.0.1:3001" target="_blank" rel="noopener noreferrer">MFE Next.js</a></li>
           <li><a href="http://127.0.0.1:3002" target="_blank" rel="noopener noreferrer">MFE Solid</a></li>
+          <li><a href="http://127.0.0.1:3003" target="_blank" rel="noopener noreferrer">MFE Svelte</a></li>  
         </ul>
       </nav>
     </div>
